cdc1686d99cea0252bc7d5c0bf05ccc7b4646f44
Shivam Jindal <= shivam.jindal@joshtechnologygroup.com>
2018-07-26 17:39:22 +0530

    use port from env

--- src/app.js
+++ src/app.js
@@ -7,6 +7,6 @@ const chalk = require('chalk');
 
 const _Port = 3001;
 
-var server = app.listen(_Port, () => console.log(chalk.magenta(`StagedFright is now running on port ${_Port}`)));
+var server = app.listen(process.env.PORT || _Port, () => console.log(chalk.magenta(`StagedFright is now running on port ${_Port}`)));
 
 module.exports = server;
