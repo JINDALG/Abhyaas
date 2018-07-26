0615550aa07a1f5b7b3f5e2f9327dd2c42ceae30
Shivam Jindal <= shivam.jindal@joshtechnologygroup.com>
2018-07-26 20:07:45 +0530

    Add bundle js

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,34 @@
 'use strict';
 
 const webpack = require('webpack');
+const path = require('path')
 
 module.exports = {
-  entry: './src/browser/react/index.js',
-  output: {
-    path: __dirname,
-    filename: './public/bundle.js'
-  },
-  context: __dirname,
-  devtool: 'source-map',
-  module: {
-    noParse: [
-      /node_modules\/aframe\/dist\/aframe-master.js/
-    ],
-    rules: [
-      {
-        test: /jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets:  [
-            'react',
-            ['es2015', { 'modules': false }],
-            'stage-2'
-          ],
-          plugins: ['transform-object-rest-spread']
-        }
-      }
-    ]
-  }
+    entry: './src/browser/react/index.js',
+    output: {
+        path: __dirname,
+        filename: 'src/browser/bundle.js',
+    },
+    context: __dirname,
+    devtool: 'source-map',
+    module: {
+        noParse: [
+            /node_modules\/aframe\/dist\/aframe-master.js/
+        ],
+        rules: [
+            {
+                test: /jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets:  [
+                        'react',
+                        ['es2015', { 'modules': false }],
+                        'stage-2'
+                    ],
+                    plugins: ['transform-object-rest-spread']
+                }
+            }
+        ]
+    }
 };
